6fabdb844c0a98a7216df79b23432d8ce3f0c3e4
Peter Carbonetto <peter.carbonetto@gmail.com>
2020-10-08 10:06:14 -0500

    Implemented script cusanovich2018_structure_plot.R.

---
+++ analysis/cusanovich2018_structure_plot.R
@@ -0,0 +1,28 @@
+# Short script to illustrate use of k-means to roughly define clusters
+# based on the topic proportions.
+library(Matrix)
+library(dplyr)
+library(fastTopics)
+library(ggplot2)
+library(cowplot)
+set.seed(1)
+
+# Load data and Poisson NMF fit.
+load("../data/Cusanovich_2018.RData")
+fit <- readRDS("../output/fit-Cusanovich2018-scd-ex-k=13.rds")$fit
+
+# Define clusters, roughly.
+clusters <- factor(kmeans(poisson2multinom(fit)$L,centers = 14)$cluster)
+print(sort(table(clusters),decreasing = TRUE))
+      
+# Create the Structure plot.
+colors <- c("#a6cee3","#1f78b4","#b2df8a","#33a02c","#fb9a99","#e31a1c",
+            "#fdbf6f","#ff7f00","#cab2d6","#6a3d9a","#ffff99","#b15928",
+            "gray")
+n <- nrow(fit$L)
+rows <- sample(n,4000)
+p1 <- structure_plot(select(poisson2multinom(fit),loadings = rows),
+                     grouping = clusters[rows],n = Inf,gap = 20,
+                     perplexity = 50,topics = 1:13,colors = colors,
+                     num_threads = 4,verbose = TRUE)
+print(p1)
