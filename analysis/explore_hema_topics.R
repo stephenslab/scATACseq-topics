1f83df51678b70e976e188a88df046153fe92b18
Peter Carbonetto <peter.carbonetto@gmail.com>
2020-11-08 21:41:59 -0600

    Fixed up PCA plots in explore_hema_topics.R.

--- analysis/explore_hema_topics.R
+++ analysis/explore_hema_topics.R
@@ -32,34 +32,27 @@ stop()
 
 samples$cluster <- factor(x)
 
-# Project the cells (otherthan the "unknown" cluster) onto the top two
-# PCs.
-facs_colors <- c("turquoise",
-                 "darkorange",
-                 "dodgerblue",
-                 "gold",
-                 "peru",
-                 "greenyellow",
-                 "firebrick",
-                 "olivedrab"
-                 ,"royalblue",
-                 "forestgreen",
-                 "gray")
+# Project the cells---other than the "unknown" cluster---onto the top
+# two PCs.
+facs_colors <- c("skyblue",     # CLP
+                 "gold",        # CMP
+                 "orange",      # GMP
+                 "forestgreen", # HSC
+                 "turquoise",   # LMPP
+                 "firebrick",     # MEP
+                 "orangered",   # mono
+                 "greenyellow", # MPP
+                 "orchid",      # pDC
+                 "gray")        # unknown
 rows <- which(samples$cluster != "U")
 fit2 <- select(poisson2multinom(fit),loadings = rows)
-p1   <- pca_plot(fit2,fill = samples[rows,"label"]) +
-  scale_fill_manual(values = topic_colors)
-p2 <- pca_hexbin_plot(poisson2multinom(fit),bins = 30,
-                      breaks = c(0,1,5,10,20,Inf))
+p1   <- pca_plot(fit2,pcs = 1:2,fill = samples[rows,"label"]) +
+  scale_fill_manual(values = facs_colors) +
+  labs(fill = "cell type")
+p2   <- pca_plot(fit2,pcs = 3:4,fill = samples[rows,"label"]) +
+  scale_fill_manual(values = facs_colors) +
+  labs(fill = "cell type")
 plot_grid(p1,p2)
-
-p3 <- pca_plot(poisson2multinom(fit),pcs = 3:4,fill = samples$label) +
-  scale_fill_manual(values = topic_colors)
-
-p4 <- pca_hexbin_plot(poisson2multinom(fit),pcs = 3:4,bins = 30,
-                      breaks = c(0,1,5,10,20,Inf))
-p5 <- pca_plot(poisson2multinom(fit),pcs = 3:4,k = 3)
-plot_grid(p3,p4,p5,nrow = 1)
 
 # Create a Structure plot.
 p6 <- structure_plot(poisson2multinom(fit),colors = topic_colors,
