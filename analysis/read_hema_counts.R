33269246407e3a784c7df5dcbf050341c39ad7ab
Peter Carbonetto <peter.carbonetto@gmail.com>
2020-11-10 09:25:40 -0600

    Added step to save data in read_hema_counts.R.

--- analysis/read_hema_counts.R
+++ analysis/read_hema_counts.R
@@ -1,8 +1,12 @@
-# TO DO: Explain here what this script does, and how to use it.
+# Short script to load the Buenrostro et al (2018) data ("Integrated
+# single-cell analysis maps the continuous regulatory landscape of
+# human hematopoietic differentiation") downloaded from Gene Omnibus,
+# accession GSE96769.
+library(tools)
 library(data.table)
 library(Matrix)
 
-# Load the fragment counts as a 2,953 x 491,437 matrix.
+# Load the fragment counts as a 2,953 x 491,437 sparse matrix.
 x <- readLines("GSE96769_scATACseq_counts.txt.gz",n = 1)
 x <- unlist(strsplit(x,"\t",fixed = TRUE))
 x <- unlist(strsplit(x,";",fixed = TRUE))
@@ -15,13 +19,15 @@ counts <- t(counts)
 rownames(counts) <- x
 rm(x,dat)
 
-# TO DO: Explain here what this code chunk is doing.
+# PLot the distribution of fragment counts.
 y <- table(summary(counts)$x)
 x <- names(y)
 y <- as.numeric(y)
-plot(x,y,pch = 20,log = "y",
+plot(x,y,pch = 20,log = "y",cex = 0.65,
      xlab = "number of fragments mapping to peak",
      ylab = "number of peaks")
 lines(x,y)
 
-# TO DO: save counts.
+# Save the counts matrix.
+save(list = "counts",file = "buenrostro2018.RData")
+resaveRdaFiles("buenrostro2018.RData")
