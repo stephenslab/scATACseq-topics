e19afe7feeaaecdfc493b02fc923e271f28803e1
Peter Carbonetto <peter.carbonetto@gmail.com>
2020-11-10 09:12:13 -0600

    Implemented steps to read counts matrix in read_hema_counts.R.

---
+++ analysis/read_hema_counts.R
@@ -0,0 +1,16 @@
+# TO DO: Explain here what this script does, and how to use it.
+library(data.table)
+library(Matrix)
+
+# Load the fragment counts as a 2,953 x 491,437 matrix.
+x <- readLines("GSE96769_scATACseq_counts.txt.gz",n = 1)
+x <- unlist(strsplit(x,"\t",fixed = TRUE))
+x <- unlist(strsplit(x,";",fixed = TRUE))
+x <- x[-1]
+dat <- fread("GSE96769_scATACseq_counts.txt.gz",sep = "\t",skip = 1)
+class(dat) <- "data.frame"
+names(dat) <- c("i","j","x")
+counts <- sparseMatrix(i = dat$i,j = dat$j,x = dat$x)
+counts <- t(counts)
+rownames(counts) <- x
+rm(x,dat)
