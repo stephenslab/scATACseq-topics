f7a8ba5d0bb7c482c001de6e6c8a4f7cd326db42
kevinlkx <kevinlkx@gmail.com>
2020-12-18 23:05:41 -0600

    transform -log10P to pvalues

--- code/motif_analysis.R
+++ code/motif_analysis.R
@@ -1,10 +1,10 @@
 
 #' Run HOMER for motif enrichment
 #' See http://homer.ucsd.edu/homer/ngs/peakMotifs.html for more details.
-#' @param path.to.homer Directory path to 'findMotifsGenome.pl' excutable file.
-#' @param regions BED file of the peak regions.
+#' @param regions.file BED file of the peak regions.
 #' @param genome Genome assembly (default='hg19').
-#' @param out.dir Output directory
+#' @param homer.path Directory path to 'findMotifsGenome.pl' excutable file.
+#' @param out.dir Output directory.
 #' @param region.size Size of the regions used for motif finding (default=200)
 #' @param motif.length Length of motifs to be found (default=8,10,12).
 #' HOMER will find motifs of each size separately and then combine the results at the end.
@@ -19,9 +19,9 @@
 #' @param n.cores Number of cores to use.
 #' @param run Whether to run this command inside R (default=TRUE)
 #' @export
-run_homer <- function(path.to.homer = 'findMotifsGenome.pl',
-                      regions = "peaks.bed",
+run_homer <- function(regions.file = "peaks.bed",
                       genome = 'hg19',
+                      homer.path = 'findMotifsGenome.pl',
                       out.dir = "out",
                       region.size = 200,
                       motif.length = c(8,10,12),
@@ -30,21 +30,12 @@ run_homer <- function(path.to.homer = 'findMotifsGenome.pl',
                       use.hypergeometric = FALSE,
                       background = NULL,
                       n.cores = 1,
-                      run = TRUE
+                      dryrun = FALSE
 ) {
 
-  path.to.homer <- normalizePath(path.to.homer)
-
-  if(!file_test('-x', path.to.homer)){
-    stop(path.to.homer, " does not exist or is not executable; check your path.to.homer parameter")
-  }
-
-  ## Make HOMER results output dir
-  dir.create(out.dir, showWarnings = FALSE, recursive = TRUE)
-
   cmd <- paste(
-    path.to.homer,
-    regions, genome, out.dir,
+    homer.path,
+    regions.file, genome, out.dir,
     '-len', paste0(motif.length, collapse = ','),
     '-size', region.size,
     '-mis', optimize.count,
@@ -60,14 +51,18 @@ run_homer <- function(path.to.homer = 'findMotifsGenome.pl',
     cmd <- paste(cmd, '-bg', background)
   }
 
-  cat("\nHOMER command: \n")
-  cat(cmd, "\n")
-
-  if(run){
+  if(dryrun){
+    cat(sprintf("HOMER command:\n%s\n", cmd))
+  }else{
+    if(!dir.exists(out.dir))
+      dir.create(out.dir, showWarnings = FALSE, recursive = TRUE)
+    homer.path <- normalizePath(homer.path)
+    if(!file_test('-x', homer.path)){
+      stop(homer.path, " does not exist or is not executable!")
+    }
     system(cmd)
-    x <- read.csv(file.pah(out.dir, "knownResults.txt"), sep="\t", header=TRUE)
-    system("rm -f *.tmp")
-    return(x)
+    res <- read.csv(file.path(out.dir, "knownResults.txt"), sep="\t", header=TRUE)
+    return(res)
   }
 
 }
@@ -117,4 +112,76 @@ run_GREAT <- function(gr,
   names(tb)
 
   return(list(job = job, tb = tb))
+}
+
+
+#' Select regions based on differential accessbility result and save selected regions as BED files.
+#' @param diff_count_res differential accessbility result from `diff_count_analysis`
+#' @param method method to select regions, `FDR` selects based on FDR, `logFC` selects based on logFC,
+#' `FDR&logFC` selects based on FDR and logFC, quantile selectes regions above abs(z-score) quantile.
+#'
+#' @param our.dir Output directory.
+#' @param thresh.FDR Threshold of FDR, default = 0.01.
+#' @param thresh.logFC Threshold of abs(logFC), default = 1.
+#' @param thresh.quantile Threshold of abs(z-score) quantile, default = 0.99.
+#' @param save.bed If TRUE, save selected regions as BED files.
+select_regions <- function(diff_count_res,
+                           method="FDR",
+                           out.dir = "out",
+                           thresh.FDR = 0.01,
+                           thresh.logFC = 1,
+                           thresh.quantile = 0.99,
+                           save.bed = TRUE) {
+
+  selected_regions <- vector("list", length = ncol(diff_count_res$Z))
+  names(selected_regions) <- colnames(diff_count_res$Z)
+  selected_regions$filenames <- c()
+  for(k in colnames(diff_count_res$Z)){
+    z <- diff_count_res$Z[,k]
+    beta <- diff_count_res$beta[,k]
+    nlog10P <- diff_count_res$pval[,k]
+    p <- 10^(-nlog10P)
+
+    if(toupper(method) == "FDR"){
+      idx_regions_sig <- which(p.adjust(p, method = "fdr") < thresh.FDR)
+      cat(sprintf("%s: %d regions with FDR < %.3f. \n", k, length(idx_regions_sig), thresh.FDR))
+    }else if(tolower(method) == "bonferroni"){
+      idx_regions_sig <- which(p.adjust(p, method = "bonferroni") < thresh.FDR)
+      cat(sprintf("%s: %d regions with Bonferroni p.adj < %.3f. \n", k, length(idx_regions_sig), thresh.FDR))
+    }else if(method == "qvalue"){
+      library(qvalue)
+      idx_regions_sig <- which(qvalue(p)$qvalues < thresh.FDR)
+      cat(sprintf("%s: %d regions with qvalue < %.3f. \n", k, length(idx_regions_sig), thresh.FDR))
+    }else if(method == "logFC"){
+      idx_regions_sig <- which(abs(beta) > thresh.logFC)
+      cat(sprintf("%s: %d regions with abs(logFC) > %.1f. \n", k, length(idx_regions_sig), thresh.logFC))
+    }else if(method == "quantile"){
+      idx_regions_sig <- which(abs(z) > quantile(abs(z), thresh.quantile))
+      cat(sprintf("%s: %d regions above quantile of %.3f. \n", k, length(idx_regions_sig), thresh.quantile))
+    }else if(method == "FDR_logFC"){
+      idx_regions_sig <- which( (p.adjust(p, method = "BH") < thresh.FDR) & (abs(beta) > thresh.logFC) )
+      cat(sprintf("%s: %d regions with FDR < %.3f AND abs(logFC) > %.3f.\n", k, length(idx_regions_sig), thresh.FDR, thresh.logFC))
+    }else{
+      stop("Method not recognized!")
+    }
+
+    if(length(idx_regions_sig) > 0){
+      region_names <- names(z)[idx_regions_sig]
+      regions <- data.frame(x = region_names)
+      regions <- regions %>% separate(x, c("chr", "start", "end"), "_")  %>% mutate_at(c("start", "end"), as.numeric)
+      regions <- data.frame(regions, name = region_names)
+      selected_regions[[k]] <- regions
+      if(save.bed){
+        # save selected marker regions as BED files
+        if(!dir.exists(out.dir))
+          dir.create(out.dir, showWarnings = FALSE, recursive = T)
+        bedfile <- paste0(out.dir, "/selected_regions_", k, ".bed")
+        write.table(regions, file = bedfile, quote=F, sep="\t", row.names=F, col.names=F)
+        selected_regions$filenames <- c(selected_regions$filenames, bedfile)
+      }
+    }
+
+  }
+
+  return(selected_regions)
 }
