2ad13f1d882e823837929f139cd63c3b3dc04179
kevinlkx <kevinlkx@gmail.com>
2022-02-25 11:15:17 -0600

    DA analysis for Buenrostro 2018 original data

--- scripts/postfit_Buenrostro2018_v2.sh
+++ scripts/postfit_Buenrostro2018_v2.sh
@@ -23,7 +23,7 @@ cd /project2/mstephens/kevinluo/scATACseq-topics/log/Buenrostro_2018/postfit_v2
 DA_DIR=${OUT_DIR}/DAanalysis-Buenrostro2018-k=10
 
 sbatch --mem=20G ~/projects/scATACseq-topics/scripts/postfit_DA_analysis.sbatch \
-       ${DAT_DIR}/Buenrostro_2018_binarized_counts.RData \
+       ${DAT_DIR}/Buenrostro_2018_binarized.RData \
        ${FIT_DIR}/fit-Buenrostro2018-binarized-scd-ex-k=10.rds \
        10000 100 ${DA_DIR}
 
@@ -37,13 +37,6 @@ sbatch --mem=30G ~/projects/scATACseq-topics/scripts/postfit_motif_analysis_v2.s
        ${DA_DIR}/DA_regions_topics_10000iters.rds \
        hg19 quantile ${MOTIFANALYSIS_DIR}
 
-## Compute motif enrichment for each topic using HOMER. Select regions by zscore
-MOTIFANALYSIS_DIR=${OUT_DIR}/motifanalysis-Buenrostro2018-k=10-zscore
-
-sbatch --mem=30G ~/projects/scATACseq-topics/scripts/postfit_motif_analysis_v2.sbatch \
-       ${DA_DIR}/DA_regions_topics_10000iters.rds \
-       hg19 zscore ${MOTIFANALYSIS_DIR}
-
 # GENE ANALYSIS
 # -------------
 ## compute gene scores using TSS based method, use original region z-scores, and normalize by the l2 norm of the weights
@@ -52,53 +45,3 @@ GENEANALYSIS_DIR=${OUT_DIR}/geneanalysis-Buenrostro2018-k=10-TSS-Z-l2
 sbatch --mem=30G ~/projects/scATACseq-topics/scripts/postfit_gene_analysis_v2.sbatch \
        ${DA_DIR}/DA_regions_topics_10000iters.rds \
        hg19 TSS none l2 ${GENEANALYSIS_DIR}
-
-## compute gene scores using TSS based method, transform region z-scores using abs(z), and normalize by the l2 norm of the weights
-GENEANALYSIS_DIR=${OUT_DIR}/geneanalysis-Buenrostro2018-k=10-TSS-absZ-l2
-
-sbatch --mem=30G ~/projects/scATACseq-topics/scripts/postfit_gene_analysis_v2.sbatch \
-       ${DA_DIR}/DA_regions_topics_10000iters.rds \
-       hg19 TSS abs l2 ${GENEANALYSIS_DIR}
-
-## compute gene scores using genebody based method, use original region z-scores, and normalize by the l2 norm of the weights
-GENEANALYSIS_DIR=${OUT_DIR}/geneanalysis-Buenrostro2018-k=10-genebody-Z-l2
-
-sbatch --mem=30G ~/projects/scATACseq-topics/scripts/postfit_gene_analysis_v2.sbatch \
-       ${DA_DIR}/DA_regions_topics_10000iters.rds \
-       hg19 genebody none l2 ${GENEANALYSIS_DIR}
-
-## compute gene scores using genebody based method, transform region z-scores using abs(z), and normalize by the l2 norm of the weights
-GENEANALYSIS_DIR=${OUT_DIR}/geneanalysis-Buenrostro2018-k=10-genebody-absZ-l2
-
-sbatch --mem=30G ~/projects/scATACseq-topics/scripts/postfit_gene_analysis_v2.sbatch \
-       ${DA_DIR}/DA_regions_topics_10000iters.rds \
-       hg19 genebody abs l2 ${GENEANALYSIS_DIR}
-
-
-## compute gene scores using TSS based method, use original region z-scores, and normalize by the sum of weights
-GENEANALYSIS_DIR=${OUT_DIR}/geneanalysis-Buenrostro2018-k=10-TSS-Z-sum
-
-sbatch --mem=30G ~/projects/scATACseq-topics/scripts/postfit_gene_analysis_v2.sbatch \
-       ${DA_DIR}/DA_regions_topics_10000iters.rds \
-       hg19 TSS none sum ${GENEANALYSIS_DIR}
-
-## compute gene scores using TSS based method, transform region z-scores using abs(z), and normalize by the sum of weights
-GENEANALYSIS_DIR=${OUT_DIR}/geneanalysis-Buenrostro2018-k=10-TSS-absZ-sum
-
-sbatch --mem=30G ~/projects/scATACseq-topics/scripts/postfit_gene_analysis_v2.sbatch \
-       ${DA_DIR}/DA_regions_topics_10000iters.rds \
-       hg19 TSS abs sum ${GENEANALYSIS_DIR}
-
-## compute gene scores using genebody based method, use original region z-scores, and normalize by the sum of weights
-GENEANALYSIS_DIR=${OUT_DIR}/geneanalysis-Buenrostro2018-k=10-genebody-Z-sum
-
-sbatch --mem=30G ~/projects/scATACseq-topics/scripts/postfit_gene_analysis_v2.sbatch \
-       ${DA_DIR}/DA_regions_topics_10000iters.rds \
-       hg19 genebody none sum ${GENEANALYSIS_DIR}
-
-## compute gene scores using genebody based method, transform region z-scores using abs(z), and normalize by the sum of weights
-GENEANALYSIS_DIR=${OUT_DIR}/geneanalysis-Buenrostro2018-k=10-genebody-absZ-sum
-
-sbatch --mem=30G ~/projects/scATACseq-topics/scripts/postfit_gene_analysis_v2.sbatch \
-       ${DA_DIR}/DA_regions_topics_10000iters.rds \
-       hg19 genebody abs sum ${GENEANALYSIS_DIR}
