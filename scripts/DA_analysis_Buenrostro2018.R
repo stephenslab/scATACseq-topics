17e9abe92839d0eb640d79267def3ea214bf4e2e
kevinlkx <kevinlkx@gmail.com>
2022-02-25 11:14:30 -0600

    DA analysis for Buenrostro 2018 data

---
+++ scripts/DA_analysis_Buenrostro2018.R
@@ -0,0 +1,55 @@
+#! /usr/bin/env Rscript
+# Perform differential accessbility (DA) analysis for Buenrostro 2018 ATAC-seq regions,
+# compute gene scores based on the weighted average of region scores.
+
+library(optparse)
+library(Matrix)
+library(fastTopics)
+
+countsfile           <- "/project2/mstephens/kevinluo/scATACseq-topics/data/Buenrostro_2018/processed_data/Buenrostro_2018_binarized.RData"
+modelfitfile         <- "/project2/mstephens/kevinluo/scATACseq-topics/output/Buenrostro_2018/binarized/fit-Buenrostro2018-binarized-scd-ex-k=10.rds"
+nc                   <- 20
+ns                   <- 10000
+nsplit               <- 100
+outdir               <- "/project2/mstephens/kevinluo/scATACseq-topics/output/Buenrostro_2018/binarized/postfit_v2/DAanalysis-Buenrostro2018-k=10"
+
+cat(sprintf("countsfile           = %s \n", countsfile))
+cat(sprintf("modelfitfile         = %s \n", modelfitfile))
+cat(sprintf("nc                   = %s \n", nc))
+cat(sprintf("ns                   = %s \n", ns))
+cat(sprintf("nsplit               = %s \n", nsplit))
+cat(sprintf("outdir              = %s \n", outdir))
+
+if(!dir.exists(outdir))
+  dir.create(outdir, showWarnings = FALSE, recursive = T)
+
+# LOAD DATA
+# ---------
+
+# Load the previously prepared count data.
+cat(sprintf("Loading data from %s.\n",countsfile))
+load(countsfile)
+cat(sprintf("Loaded %d x %d counts matrix.\n",nrow(counts),ncol(counts)))
+
+# LOAD MODEL FIT
+# --------------
+cat(sprintf("Loading Poisson NMF model fit from %s\n",modelfitfile))
+fit <- readRDS(modelfitfile)$fit
+
+# COMPUTE REGION SCORES USING DIFFERENTIAL ANALYSIS
+# -------------------------------------------------
+# Perform differential accessbility analysis using the multinomial topic model.
+outfile <- file.path(outdir, paste0("DA_regions_topics_", ns,"iters.rds"))
+
+cat("Computing differential accessbility statistics from topic model.\n")
+cat("Run", ns, "iterations of MCMC...\n")
+timing <- system.time(
+  DA_res <- de_analysis(fit,counts,pseudocount = 0.1,
+                        control = list(ns = ns,nc = nc,nsplit = nsplit)))
+cat(sprintf("Computation took %0.2f seconds.\n",timing["elapsed"]))
+
+cat("Saving results.\n")
+saveRDS(DA_res, outfile)
+
+
+sessionInfo()
