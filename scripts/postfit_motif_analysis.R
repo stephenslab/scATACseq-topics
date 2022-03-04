4f93e8395a52a11170f1b200ac0d925273f0d1b0
kevinlkx <kevinlkx@gmail.com>
2022-03-04 10:43:32 -0600

    use pvalue to select regions for motif enrichment analysis and do not remove regions with NAs in z-scores

--- scripts/postfit_motif_analysis.R
+++ scripts/postfit_motif_analysis.R
@@ -14,31 +14,20 @@ source("code/motif_analysis.R")
 parser <- OptionParser()
 parser <- add_option(parser,"--DAfile",type="character",default="DA_res.rds")
 parser <- add_option(parser,"--genome",type="character",default="hg19")
-parser <- add_option(parser,"--selectmethod",type="character",default="quantile")
 parser <- add_option(parser,"--homerpath",type="character",default="findMotifsGenome.pl")
 parser <- add_option(parser,"--nc",type = "integer",default = 1)
 parser <- add_option(parser,c("--out","-o"),type="character",default="out")
 out    <- parse_args(parser)
 DAfile          <- out$DAfile
 genome          <- out$genome
-selectmethod    <- out$selectmethod
 homerpath       <- out$homerpath
 nc              <- out$nc
 out.dir         <- out$out
 rm(parser,out)
 
-## Example settings
-# DAfile       <- '/project2/mstephens/kevinluo/scATACseq-topics/output/Buenrostro_2018_Chen2019pipeline/binarized/postfit_v2/DAanalysis-Buenrostro2018-k=11-quantile-v2/DA_regions_topics_ns1000.rds'
-# genome          <- 'hg19'
-# selectmethod    <- 'lfsr'
-# homerpath       <- '/project2/xinhe/software/homer/bin/findMotifsGenome.pl'
-# nc              <- 8
-# out.dir         <- '/project2/mstephens/kevinluo/scATACseq-topics/output/Buenrostro_2018_Chen2019pipeline/binarized/postfit_v2/motifanalysis-Buenrostro2018-k=11-quantile'
-
 cat(sprintf("DAfile    = %s \n", DAfile))
 cat(sprintf("genome       = %s \n", genome))
 cat(sprintf("homerpath    = %s \n", homerpath))
-cat(sprintf("selectmethod = %s \n", selectmethod))
 cat(sprintf("nc           = %s \n", nc))
 cat(sprintf("out.dir      = %s \n", out.dir))
 
@@ -58,25 +47,30 @@ if(!file.exists(DAfile)){
 cat("Load precomputed differential accessbility statistics.\n")
 DA_res <- readRDS(DAfile)
 
-# Filter out regions with NAs
-DA_res <- DA_res[c("postmean", "z", "lfsr")]
+# Regions with NAs
 rows_withNAs <- which(apply(DA_res$z, 1, anyNA))
-cat("Filter out", length(rows_withNAs), "regions with NAs... \n")
-DA_res$z <- DA_res$z[-rows_withNAs,]
-DA_res$postmean <- DA_res$postmean[-rows_withNAs,]
-DA_res$lfsr <- DA_res$lfsr[-rows_withNAs,]
+cat(length(rows_withNAs), "regions with NAs in z-scores... \n")
+head(DA_res$z[rows_withNAs,])
 
-# SELECT REGIONS FOR MOTIF ENRICHMENT ANALYSIS
-# --------------------------------------------
-cat("Select regions. \n")
-homer.dir <- paste0(out.dir, "/HOMER/")
+# Filter out regions with NAs in z-scores
+# DA_res$z <- DA_res$z[-rows_withNAs,]
+# DA_res$postmean <- DA_res$postmean[-rows_withNAs,]
+# DA_res$lpval <- DA_res$lpval[-rows_withNAs,]
+# DA_res$lfsr <- DA_res$lfsr[-rows_withNAs,]
+
+
+# SELECT REGIONS and PERFORM MOTIF ENRICHMENT ANALYSIS USING HOMER
+# -----------------------------------------------------------------
+
+# Select regions with DA p-value < 0.1
+# -------------------------------------
+homer.dir <- paste0(out.dir, "/HOMER/DA_pval_0.1_regions")
+cat("Select regions for motif enrichment analysis... \n")
 cat(sprintf("%d regions in total. \n", nrow(DA_res$z)))
 
-selected_regions <- select_DA_regions(DA_res, method = selectmethod, out.dir = homer.dir, save.bed = TRUE)
+selected_regions <- select_DA_regions(DA_res, method = "pval", thresh.pval = 0.1, out.dir = homer.dir, save.bed = TRUE)
 saveRDS(selected_regions, paste0(homer.dir, "/selected_regions.rds"))
 
-# PERFORM MOTIF ENRICHMENT ANALYSIS USING HOMER
-# ---------------------------------------------
 # For each topic, perform TF motif enrichment analysis using HOMER hypergeometric test.
 cat("Performing motif enrichment analysis using HOMER.\n")
 homer_res <- vector("list", ncol(DA_res$z))
@@ -91,4 +85,29 @@ for(k in 1:ncol(DA_res$z)){
 }
 saveRDS(homer_res, paste0(homer.dir, "/homer_knownResults.rds"))
 
-# sessionInfo()
+
+# Select regions with DA p-value < 0.05
+# --------------------------------------
+homer.dir <- paste0(out.dir, "/HOMER/DA_pval_0.05_regions")
+cat("Select regions for motif enrichment analysis... \n")
+cat(sprintf("%d regions in total. \n", nrow(DA_res$z)))
+
+selected_regions <- select_DA_regions(DA_res, method = "pval", thresh.pval = 0.05, out.dir = homer.dir, save.bed = TRUE)
+saveRDS(selected_regions, paste0(homer.dir, "/selected_regions.rds"))
+
+# For each topic, perform TF motif enrichment analysis using HOMER hypergeometric test.
+cat("Performing motif enrichment analysis using HOMER.\n")
+homer_res <- vector("list", ncol(DA_res$z))
+names(homer_res) <- colnames(DA_res$z)
+for(k in 1:ncol(DA_res$z)){
+  homer_res[[k]] <- run_homer(selected_regions$filenames[k],
+                              genome = genome,
+                              homer.path = homerpath,
+                              use.hypergeometric = TRUE,
+                              out.dir=paste0(homer.dir, "/homer_result_topic_", k),
+                              n.cores=nc)
+}
+saveRDS(homer_res, paste0(homer.dir, "/homer_knownResults.rds"))
+
+
+sessionInfo()
