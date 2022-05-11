95c9bea7c95f78cf57c69b54882e19bd320eb720
Peter Carbonetto <peter.carbonetto@gmail.com>
2022-05-11 15:42:42 -0500

    Added some comments to top of homer_buenrostro2018_k10.R script.

--- scripts/homer_buenrostro2018_k10.R
+++ scripts/homer_buenrostro2018_k10.R
@@ -1,8 +1,14 @@
-# TO DO: Explain here what this script does, and how to use it.
+# A short script for performing HOMER motif enrichment analysis using
+# the differential accessibility results generated from the
+# de_analysis_buenrostro2018_k10.R script. These were the steps taken
+# to load R and allocate computing resources for this analysis:
 #
 #   sinteractive -p broadwl -c 4 --mem=16G --time=12:00:00
 #   module load R/3.5.1
 #
+# Also for this script to work the findMotifsGenome.pl Perl script
+# needs to be findable (e.g., by adding the appropriate directory to
+# the PATH environment variable).
 library(tools)
 library(fastTopics)
 
