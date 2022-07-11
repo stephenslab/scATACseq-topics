18fd9b28a195b117f3bdf9bb934644be9ee9e83e
Peter Carbonetto <peter.carbonetto@gmail.com>
2022-07-11 09:25:03 -0500

    Made first draft of tile plot for buenrostro et al motif enrichment results.

--- analysis/temp.R
+++ analysis/temp.R
@@ -1,3 +1,6 @@
+library(ggplot2)
+library(cowplot)
+
 # Draft analysis of motifs in topics k = 1 through 10.
 homer <- readRDS(file.path("../output/Buenrostro_2018",
                            "binarized/filtered_peaks",
@@ -19,3 +22,31 @@ for (i in 1:k) {
 }
 pvals <- as.data.frame(pvals)
 
+# For each topic, pick the top 10 motifs by p-value.
+motifs <- NULL
+for (i in 1:k) {
+  rows1  <- which(pvals[,i] <= 0)
+  rows2  <- order(pvals[,i])
+  motifs <- c(motifs,rownames(pvals)[c(rows1,rows2[1:10])])
+}
+motifs <- sort(unique(motifs))
+pvals <- pvals[motifs,]
+
+# Plot the p-values in a tile plot.
+# Colors from colorbrewer2.org.
+colors <- c("#d73027","#f46d43","#fdae61","#fee090",
+            "#e0f3f8","#abd9e9","#74add1",",#4575b4")
+pdat <- NULL
+for (i in 1:k) {
+  pdat <- rbind(pdat,
+                data.frame(motif = motifs,
+                           topic = paste0("k",i),
+                           lpval = log10(pvals[,i] + 1e-256)))
+}
+pdat <- transform(pdat,
+                  lpval = cut(lpval,seq(-257,0,length.out = 8)),
+                  motif = factor(motif,rev(motifs)))
+p <- ggplot(pdat,aes(x = topic,y = motif,fill = lpval)) +
+  geom_tile(color = "white",size = 0.5) +
+  scale_fill_manual(values = colors) +
+  theme_cowplot(font_size = 8)
