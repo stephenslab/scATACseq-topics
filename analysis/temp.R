14ec480bf9b9f5011d974e3f6d3c649d8116c223
Peter Carbonetto <peter.carbonetto@gmail.com>
2022-07-29 13:20:23 -0500

    Added second structure plot in which cells are arranged by samples.

--- analysis/temp.R
+++ analysis/temp.R
@@ -2,13 +2,18 @@ library(Matrix)
 library(fastTopics)
 set.seed(1)
 load("data/Cusanovich_2018/processed_data/Cusanovich_2018_metadata_only.RData")
-fit <- readRDS("output/Cusanovich_2018/fit-Cusanovich2018-scd-ex-k=11.rds")$fit
-samples <- transform(samples,tissue.replicate = factor(tissue.replicate))
+fit <- readRDS("output/Cusanovich_2018/fit-Cusanovich2018-scd-ex-k=13.rds")$fit
+samples <- transform(samples,
+                     tissue.replicate = factor(tissue.replicate),
+                     cell_label = factor(cell_label))
 topic_colors <- c("#a6cee3","#1f78b4","#b2df8a","#33a02c","#fb9a99","#e31a1c",
                   "#fdbf6f","#ff7f00","#cab2d6","#6a3d9a","#ffff99","#b15928",
                   "gray","darkblue","magenta")
 pca_embed_method <- function (fit, ...)
   drop(pca_from_topics(fit,dims = 1))
-p <- structure_plot(fit,grouping = samples$tissue.replicate,gap = 20,n = 4000,
+p1 <- structure_plot(fit,grouping = samples$tissue.replicate,gap = 20,n = 4000,
+                    perplexity = 70,colors = topic_colors,verbose = FALSE,
+                    embed_method = pca_embed_method)
+p2 <- structure_plot(fit,grouping = samples$cell_label,gap = 10,n = 4000,
                     perplexity = 70,colors = topic_colors,verbose = FALSE,
                     embed_method = pca_embed_method)
