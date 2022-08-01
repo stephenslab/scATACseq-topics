1bf4c5059bf75e4776e7bbf687ac0e7a02ac2457
Peter Carbonetto <peter.carbonetto@gmail.com>
2022-08-01 07:10:25 -0500

    Updated .gitignore.

--- analysis/temp.R
+++ analysis/temp.R
@@ -1,8 +1,3 @@
-library(Matrix)
-library(fastTopics)
-set.seed(1)
-load("data/Cusanovich_2018/processed_data/Cusanovich_2018_metadata_only.RData")
-fit <- readRDS("output/Cusanovich_2018/fit-Cusanovich2018-scd-ex-k=13.rds")$fit
 x <- samples$cell_label
 x[x == "Activated B cells"] <- "B cells"
 x[x == "Immature B cells"] <- "B cells"
@@ -51,17 +46,7 @@ cell_types <-
     "Collisions",
     "Unknown")
 samples <- transform(samples,
-                     tissue.replicate = factor(tissue.replicate),
                      cell_label = factor(x,cell_types))
-topic_colors <- c("#a6cee3","#1f78b4","#b2df8a","#33a02c","#fb9a99","#e31a1c",
-                  "#fdbf6f","#ff7f00","#cab2d6","#6a3d9a","#ffff99","#b15928",
-                  "gray","darkblue","magenta")
-pca_embed_method <- function (fit, ...)
-    drop(pca_from_topics(fit,dims = 1))
-
-# TO DO: Fix topic colours and order of tissues in first Structure plot.
-p1 <- structure_plot(fit,grouping = samples$tissue.replicate,gap = 20,n = 4000,
-                     perplexity = 30,colors = topic_colors,verbose = FALSE)
 p2 <- structure_plot(fit,grouping = samples$cell_label,gap = 20,n = 4000,
                     perplexity = 30,colors = topic_colors,verbose = FALSE)
 print(p2)
