f338d8d5b971c40118bd0501ef237b8e55fac71e
Peter Carbonetto <peter.carbonetto@gmail.com>
2022-08-04 22:21:46 -0500

    Created second structure plot using our own clustering.

--- analysis/cusanovich2018_kidney_k10.R
+++ analysis/cusanovich2018_kidney_k10.R
@@ -1,8 +1,11 @@
 library(Matrix)
 library(fastTopics)
+library(ggplot2)
+library(cowplot)
 load("data/Cusanovich_2018/processed_data/Cusanovich_2018_Kidney.RData")
 fit <- readRDS(file.path("output/Cusanovich_2018/tissues",
                          "fit-Cusanovich2018-Kidney-scd-ex-k=10.rds"))$fit
+fit <- poisson2multinom(fit)
 
 # Plot using Cusanovich et al labeling.
 set.seed(1)
@@ -26,3 +29,28 @@ topic_colors <- c("peru","darkmagenta","magenta","gold","darkorange",
 p1 <- structure_plot(fit,grouping = samples$cell_label,gap = 20,
                      colors = topic_colors,verbose = FALSE)
 print(p1)
+
+# Cluster the cells using the topic proportions only.
+pca <- prcomp(fit$L)$x
+pc1 <- pca[,1]
+pc2 <- pca[,2]
+x <- rep("other",nrow(pca))
+x[pc1 < 0] <- "proximal tubule"
+x[pc1 > 0 & pc2 > 0.2] <- "endothelial I"
+rows <- which(x == "other")
+fit2 <- select_loadings(fit,loadings = rows)
+pca  <- prcomp(fit2$L)$x
+y    <- rep("DCT + CD",nrow(pca))
+pc1  <- pca[,1]
+pc2  <- pca[,2]
+y[pc1 < 0 & pc2 > -0.15] <- "loop of henle"
+y[pc1 >= 0 & pc2 > -0.15] <- "podocytes + other"
+x[rows] <- y
+samples$cluster <- factor(x,c("DCT + CD","loop of henle","proximal tubule",
+                              "podocytes + other","endothelial I"))
+
+# Plot using our clustering.
+set.seed(1)
+p2 <- structure_plot(fit,grouping = samples$cluster,gap = 20,
+                     colors = topic_colors,verbose = FALSE)
+print(p2)
