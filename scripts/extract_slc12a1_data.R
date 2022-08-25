62f598ed1198ecc4c9784e5cb1197165c58978c2
Peter Carbonetto <peter.carbonetto@gmail.com>
2022-08-25 09:43:20 -0500

    Generated slc12a1_data.RData data file.

--- scripts/extract_slc12a1_data.R
+++ scripts/extract_slc12a1_data.R
@@ -1,5 +1,18 @@
 # TO DO: Explain here what this script is for, and how to use it.
 library(Matrix)
+library(tools)
 
-# Load the gene activity scores.
+# Load the previously prepared count data.
+load(file.path("../data/Cusanovich_2018/processed_data",
+               "Cusanovich_2018_Kidney.RData"))
+ids <- rownames(counts)
 
+# Load the gene activity scores downloaded from the Mouse sci-ATAC-seq
+# Atlas website.
+scores <- readRDS(file.path("../data/Cusanovich_2018/processed_data",
+                            "activity_scores.quantitative.rds"))
+scores <- scores["SLC12A1",ids]
+
+# Save these data to an .RData file.
+save(list = "scores",file = "slc12a1_data.RData")
+resaveRdaFiles("slc12a1_data.RData")
