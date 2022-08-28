19f5e9de0950c0b24b4ad16f4b3e42c22d5cc631
Peter Carbonetto <peter.carbonetto@gmail.com>
2022-08-28 11:02:05 -0500

    Added some plots to explore_cicero.R showing de_analysis results before and after shrinkage.

--- analysis/explore_cicero.R
+++ analysis/explore_cicero.R
@@ -1,5 +1,6 @@
 library(fastTopics)
 library(pathways)
+library(ashr)
 library(ggplot2)
 library(cowplot)
 
@@ -8,6 +9,9 @@ load("data/mm9_seq_gene.RData")
 
 # Load the data on gene Slc12a1.
 load("data/Cusanovich_2018/processed_data/slc12a1_data.RData")
+cicero <- transform(cicero,
+                    Peak1 = as.character(Peak1),
+                    Peak2 = as.character(Peak2))
 
 # Load the K = 10 topic model fit.
 fit <- readRDS(file.path("output/Cusanovich_2018/tissues",
@@ -43,17 +47,35 @@ p1 <- ggplot(pdat,aes(x = loading,y = score)) +
   theme_cowplot()
 
 # Here let's examine the de_analysis results for the peaks near gene
-# Slc12a1.
+# Slc12a1; specifically, within 400 kb of the transcribed region of
+# gene Slc12a1.
 d <- 4e5
-load("data/mm9_seq_gene.RData")
 seq_gene <- subset(seq_gene,feature_name == "Slc12a1")
-rows <- which(positions$chr == paste0("chr",seq_gene$chromosome) &
-              positions$start > seq_gene$chr_start - d &
-              positions$end < seq_gene$chr_stop + d)
-peaks <- c(as.character(cicero$Peak1),as.character(cicero$Peak2))
+rows <- which(with(positions,chr == paste0("chr",seq_gene$chromosome) &
+                   start > seq_gene$chr_start - d &
+                   end < seq_gene$chr_stop + d))
+peaks <- c(cicero$Peak1,cicero$Peak2)
+pdat <- data.frame(start      = positions[rows,"start"]/1e5,
+                   postmean   = de$postmean[rows,"k8"],
+                   z          = de$z[rows,"k8"],
+                   lpval      = de$lpval[rows,"k8"],
+                   cicero_hit = is.element(positions[rows,"name"],peaks))
+p1 <- ggplot(pdat,aes(x = start,y = lpval,color = cicero_hit)) +
+  geom_point() +
+  scale_color_manual(values = c("darkblue","magenta")) +
+  theme_cowplot()
+p2 <- ggplot(pdat,aes(x = start,y = postmean,color = cicero_hit)) +
+  geom_point() +
+  geom_hline(yintercept = 0,color = "black",linetype = "dashed") +
+  scale_color_manual(values = c("darkblue","magenta")) +
+  theme_cowplot()
+plot_grid(p1,p2,nrow = 2,ncol = 1)
 
-x <- factor(is.element(positions[rows,"name"],peaks))
-levels(x) <- c("dodgerblue","darkorange")
-x <- as.character(x)
-plot(positions[rows,"start"],de$lpval[rows,"k8"],pch = 20,col = x)
-plot(positions[rows,"start"],de$postmean[rows,"k8"],pch = 20)
+# Run ashr on these results.
+pdat <- transform(pdat,se = postmean/z)
+fit <- ash(pdat$postmean,pdat$se,mixcompdist = "normal",method = "shrink")
+pdat$ash <- fit$result$PosteriorMean
+ggplot(pdat,aes(x = postmean,y = ash,color = se)) +
+  geom_point() +
+  geom_abline(intercept = 0,slope = 1,color = "black",linetype = "dashed") +
+  theme_cowplot()
