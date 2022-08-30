3f51bb1ab553faf50485db04b657c39d7b0d7e1a
Peter Carbonetto <peter.carbonetto@gmail.com>
2022-08-30 07:22:40 -0500

    Removed explore_cicero.R.

--- analysis/explore_cicero.R
+++
@@ -1,8 +0,0 @@
-# Run ashr on these results.
-pdat <- transform(pdat,se = postmean/z)
-fit <- ash(pdat$postmean,pdat$se,mixcompdist = "normal",method = "shrink")
-pdat$ash <- fit$result$PosteriorMean
-ggplot(pdat,aes(x = postmean,y = ash,color = se)) +
-  geom_point() +
-  geom_abline(intercept = 0,slope = 1,color = "black",linetype = "dashed") +
-  theme_cowplot()
