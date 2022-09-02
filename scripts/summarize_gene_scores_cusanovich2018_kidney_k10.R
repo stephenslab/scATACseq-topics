58ad31668d53da28fb633603b294f0202ba5556e
Peter Carbonetto <peter.carbonetto@gmail.com>
2022-09-01 22:36:41 -0500

    Added first steps to set up data structure in summarize_gene_scores_cusanovich2018_kidney_k10.R.

--- scripts/summarize_gene_scores_cusanovich2018_kidney_k10.R
+++ scripts/summarize_gene_scores_cusanovich2018_kidney_k10.R
@@ -1,3 +1,12 @@
 # TO DO: Explain here what this script is for, and how to use it.
-library(tools)
 library(fastTopics)
+library(tools)
+load(file.path("../output/Cusanovich_2018/tissues",
+               "gene-scores-cusanovich2018-kidney-k=10.RData"))
+k <- 10
+n <- length(gene_scores)
+A <- matrix(as.numeric(NA),n,k)
+rownames(A) <- names(gene_scores)
+colnames(A) <- paste0("k",1:k)
+de_gene <- list(postmean = A,se = A,z = A,lfsr = A)
+class(de) <- c("topic_model_de_analysis","list")
