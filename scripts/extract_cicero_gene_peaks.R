f38c599b487d28bc26b72bb8ee7d6add0882e7f4
Peter Carbonetto <peter.carbonetto@gmail.com>
2022-09-21 09:05:31 -0500

    Updated comments in extract_cicero_gene_peaks.R.

--- scripts/extract_cicero_gene_peaks.R
+++ scripts/extract_cicero_gene_peaks.R
@@ -2,7 +2,7 @@
 # containing a list of peaks for each gene.
 library(tools)
 
-# Get the peaks linked to each gene.
+# Import the Cicero co-accessibility data.
 cicero <- readRDS(file.path("../data/Cusanovich_2018/processed_data",
                             "master_cicero_conns.rds"))
 class(cicero) <- "data.frame"
@@ -14,10 +14,16 @@ cicero  <- transform(cicero,
                      Peak2             = as.character(Peak2),
                      peak1.tss.gene_id = factor(peak1.tss.gene_id),
                      peak2.tss.gene_id = factor(peak2.tss.gene_id))
-# cicero_gene <- tapply(cicero$Peak1,cicero$peak1.tss.gene_id,unique,
-#                       simplify = FALSE)
+
+# Here we extract, for each gene, the distal and proximal sites that
+# are connected to Peak1. The two columns used in this calculation
+# are: (1) "peak1.tss.gene_id", Ensemble gene id of the proximal TSS
+# overlapping Peak1; (2) "Peak2", peak id of the second peak in the
+# connection.
 cicero_gene <- tapply(cicero$Peak2,cicero$peak1.tss.gene_id,unique,
                       simplify = FALSE)
+# cicero_gene <- tapply(cicero$Peak1,cicero$peak1.tss.gene_id,unique,
+#                       simplify = FALSE)
 
 # Save these data to an .RData file.
 save(list = "cicero_gene",file = "cicero_gene.RData")
