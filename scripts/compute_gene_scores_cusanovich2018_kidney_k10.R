e18bdffa6cc88df079da7d08899120b760a8410b
Peter Carbonetto <peter.carbonetto@gmail.com>
2022-09-21 09:25:29 -0500

    Revised initial call to ash in compute_gene_scores_cusanovich2018_kidney_k10.R.

--- scripts/compute_gene_scores_cusanovich2018_kidney_k10.R
+++ scripts/compute_gene_scores_cusanovich2018_kidney_k10.R
@@ -19,23 +19,14 @@ load("../data/cicero_gene.RData")
 load(file.path("../output/Cusanovich_2018/tissues",
                "de-cusanovich2018-kidney-k=10-noshrink.RData"))
 
-# Run ash once on all peaks for a selected topic to obtain a default
-# prior. Here we use topic k = 8
-# because it seems to correspond well to the Loop of Henle (LoH) cell
-# type.
-j <- "k8"
-b <- de$postmean[,j]
-z <- de$z[,j]
+# Run ash once on all peaks and all topics.
+b  <- as.vector(de$postmean)
+z  <- as.vector(de$z)
 se <- b/z
-res0 <- ash(b,se,mixcompdist = "normal",method = "shrink")
+res0 <- ash(b,se,mixcompdist = "normal",method = "shrink",outputlevel = 1)
 
-# res1 <- ash(b[1:100],se[1:100],mixcompdist = "normal",method = "shrink",
-#             g = res$fitted_g,fixg = TRUE)
-# res2 <- ash(b[1:100],se[1:100],mixcompdist = "normal",method = "shrink",
-#             g = res$fitted_g)
-
-# For each gene, perform adaptive shrinkage on the de_analysis
-# results for all peaks near the gene.
+# For each gene and each topic, perform adaptive shrinkage on the
+# de_analysis results for all peaks near the gene.
 peaks <- rownames(de$postmean)
 genes <- names(cicero_gene)
 gene_scores <- vector("list",length(genes))
@@ -46,7 +37,7 @@ for (gene in genes) {
   # Get the peaks near the gene.
   rows <- which(is.element(peaks,cicero_gene[[gene]]))
   if (length(rows) > 0) {
-      
+
     # Set up the ash inputs.
     b  <- de$postmean[rows,,drop = FALSE]
     z  <- de$z[rows,,drop = FALSE]
