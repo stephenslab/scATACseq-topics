c1c40bfb165612720fd38acc0d628f3e96b9aff4
Peter Carbonetto <peter.carbonetto@gmail.com>
2022-09-23 08:44:42 -0500

    In the extract_cicero_gene_peaks.R script, the cicero_gene data is now stored as a sparse matrix.

--- scripts/extract_cicero_gene_peaks.R
+++ scripts/extract_cicero_gene_peaks.R
@@ -11,8 +11,8 @@ cicero <- cicero[c("Peak1","Peak2","peak1.tss.gene_id",
                    "peak2.tss.gene_id","cluster")]
 cicero <- subset(cicero,is.element(cluster,c(11,18,22,25)))
 cicero <- transform(cicero,
-                    Peak1             = as.character(Peak1),
-                    Peak2             = as.character(Peak2),
+                    Peak1             = factor(as.character(Peak1)),
+                    Peak2             = factor(as.character(Peak2)),
                     peak1.tss.gene_id = factor(peak1.tss.gene_id),
                     peak2.tss.gene_id = factor(peak2.tss.gene_id))
 
@@ -21,16 +21,30 @@ cicero <- transform(cicero,
 # are: (1) "peak1.tss.gene_id", Ensemble gene id of the proximal TSS
 # overlapping Peak1; (2) "Peak2", peak id of the second peak in the
 # connection.
-#
-# TO DO:Encode these data as a sparse matrix.
-#
+genes <- levels(cicero$peak1.tss.gene_id)
+peaks <- levels(cicero$Peak2)
+cicero <- transform(cicero,
+                    Peak1 = as.numeric(Peak1),
+                    Peak2 = as.numeric(Peak2))
 cicero_gene <- tapply(cicero$Peak2,cicero$peak1.tss.gene_id,unique,
                       simplify = FALSE)
 # cicero_gene <- tapply(cicero$Peak1,cicero$peak1.tss.gene_id,unique,
 #                       simplify = FALSE)
-j <- unlist
-sparseMatrix(i = i,j = j,dims = )
+
+# Encode these data as a sparse matrix cicero_gene_mat such that
+# cicero_gene_mat[i,j] = 1 if and only if peak i is connected to gene j.
+n <- length(peaks)
+m <- length(genes)
+j <- cicero_gene
+for (t in 1:m)
+  j[[t]][] <- t
+i <- unlist(cicero_gene)
+j <- unlist(j)
+cicero_gene_mat <- sparseMatrix(i = i,j = j,dims = c(n,m))
+cicero_gene_mat <- as(cicero_gene_mat,"dgCMatrix")
+rownames(cicero_gene_mat) <- peaks
+colnames(cicero_gene_mat) <- genes
 
 # Save these data to an .RData file.
-save(list = "cicero_gene",file = "cicero_gene_kidney.RData")
+save(list = "cicero_gene_mat",file = "cicero_gene_kidney.RData")
 resaveRdaFiles("cicero_gene_kidney.RData")
