bf372aaa1d24ee53dfe9c168b1308f1a535468d0
Peter Carbonetto <peter.carbonetto@gmail.com>
2023-05-09 10:14:13 -0500

    Revised de_analysis_cusanovich2018_kidney_k10.R script to use binomial topic model.

--- scripts/de_analysis_cusanovich2018_kidney_k10.R
+++ scripts/de_analysis_cusanovich2018_kidney_k10.R
@@ -1,7 +1,7 @@
-# A short script used to perform the differential expression (DE)
-# analysis using the multinomial topic model fitted to the Buenrostro
-# et al (2018) data, with k = 10 topics. These were the steps taken to
-# load R and allocate computing resources for this analysis:
+# Perform GoM DE analysis using the binomial topic model fitted to the
+# Cusanovich et al (2018) kidney data, with k = 10 topics. These were
+# the steps taken to load R and allocate computing resources for this
+# analysis:
 #
 #   sinteractive -p broadwl -c 8 --mem=32G --time=80:00:00
 #   module load R/3.5.1
@@ -20,14 +20,12 @@ load(file.path("../data/Cusanovich_2018/processed_data",
                "Cusanovich_2018_Kidney.RData"))
 
 # Load the k = 10 Poisson NMF model fit.
-fit <- readRDS(
-  file.path("../output/Cusanovich_2018/tissues",
-            "fit-Cusanovich2018-Kidney-scd-ex-k=10.rds"))$fit
-fit <- poisson2multinom(fit)
+load(file.path("../output/Cusanovich_2018/tissues",
+               "binom-fit-cusanovich2018-kidney-k=10.RData"))
 
 # Perform the DE analysis.
 t0 <- proc.time()
-de <- de_analysis(fit,counts,shrink.method = "none",pseudocount = 0.1,
+de <- de_analysis(fit_binom,counts,shrink.method = "none",pseudocount = 0.1,
                   control = list(ns = 1e5,nc = 8))
 t1 <- proc.time()
 timing <- t1 - t0
