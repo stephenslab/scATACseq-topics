3381c79ffaa0ade96310b7808890bf7eba3c1dda
Peter Carbonetto <peter.carbonetto@gmail.com>
2023-05-09 09:33:40 -0500

    Updated de_analysis_buenrostro2018_k10.R to use binomial topic model.

--- scripts/de_analysis_buenrostro2018_k10.R
+++ scripts/de_analysis_buenrostro2018_k10.R
@@ -1,7 +1,7 @@
-# A short script used to perform the differential expression (DE)
-# analysis using the multinomial topic model fitted to the Buenrostro
-# et al (2018) data, with k = 10 topics. These were the steps taken to
-# load R and allocate computing resources for this analysis:
+# Perform GoM DE analysis using the binomial topic model fitted to the
+# Buenrostro et al (2018) data, with k = 10 topics. These were the
+# steps taken to load R and allocate computing resources for this
+# analysis:
 #
 #   sinteractive -p broadwl -c 8 --mem=16G --time=36:00:00
 #   module load R/3.5.1
@@ -19,15 +19,13 @@ set.seed(1)
 load(file.path("../output/Buenrostro_2018/binarized/filtered_peaks",
                "Buenrostro_2018_binarized_filtered.RData"))
 
-# Load the k = 10 Poisson NMF model fit.
-fit <- readRDS(
-  file.path("../output/Buenrostro_2018/binarized/filtered_peaks",
-            "fit-Buenrostro2018-binarized-filtered-scd-ex-k=10.rds"))$fit
-fit <- poisson2multinom(fit)
+# Load the k = 10 binomial topic model fit.
+load(file.path("../output/Buenrostro_2018/binarized/filtered_peaks",
+               "binom-fit-buenrostro2018-k=10.RData"))
 
 # Perform the DE analysis.
 t0 <- proc.time()
-de <- de_analysis(fit,counts,shrink.method = "none",pseudocount = 0.1,
+de <- de_analysis(fit_binom,counts,shrink.method = "none",pseudocount = 0.1,
                   control = list(ns = 1e5,nc = 8))
 t1 <- proc.time()
 timing <- t1 - t0
